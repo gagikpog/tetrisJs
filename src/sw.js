f7c2d9881d888494491289fe6006ae1b1ca5b843
Gagik <gagikpog@gmail.com>
2022-06-03 23:10:07 +0500

    new servise worker

--- src/sw.js
+++ src/sw.js
@@ -1,69 +1,53 @@
-/*
- Copyright 2016 Google Inc. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
+let debugMode = false;
+const RUNTIME = 'cache-version-1';
 
-// Names of the two caches used in this version of the service worker.
-// Change to v2, etc. when you update any of the local resources, which will
-// in turn trigger the install event again.
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
+function log(msg) {
+    if (debugMode) {
+        console.log(msg);
+    }
+}
 
-// A list of local resources we always want to be cached.
-const PRECACHE_URLS = [];
+function loadFromOrigin(request) {
+    log('load file: ' + request.url);
+    return fetch(request);
+}
 
-// The install handler takes care of precaching the resources we always need.
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(PRECACHE)
-            .then(cache => cache.addAll(PRECACHE_URLS))
-            .then(self.skipWaiting())
-    );
-});
+function addToCache(request, response) {
+    log('add to cache: ' + request.url);
+    return caches.open(RUNTIME).then((cache) => cache.put(request, response.clone())).then(() => response);
+}
+
+function loadFromCache(request) {
+    return caches.match(request).then((cachedResponse) => {
+        if (cachedResponse) {
+            log('load from cache: ' + request.url);
+            return cachedResponse;
+        } else {
+            log('failed load from cache: ' + request.url);
+            throw new Error('Response not cached');
+        }
+    });
+}
 
 // The activate handler takes care of cleaning up old caches.
 self.addEventListener('activate', (event) => {
-    const currentCaches = [PRECACHE, RUNTIME];
+    const currentCaches = [RUNTIME];
     event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-        }).then(cachesToDelete => {
-            return Promise.all(cachesToDelete.map(cacheToDelete => {
-                return caches.delete(cacheToDelete);
-            }));
-        }).then(() => self.clients.claim())
+        caches.keys().then(cacheNames =>
+            cacheNames.filter(cacheName => !currentCaches.includes(cacheName))
+        ).then(cachesToDelete => 
+            Promise.all(cachesToDelete.map(cacheToDelete => caches.delete(cacheToDelete)))
+        ).then(() => self.clients.claim())
     );
 });
 
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
 self.addEventListener('fetch', (event) => {
-    // Skip cross-origin requests, like those for Google Analytics.
-    if (event.request.url.startsWith(self.location.origin)) {
+    // ignore chrome extensions
+    if (!event.request.url.startsWith('chrome')) {
         event.respondWith(
-            caches.match(event.request).then((cachedResponse) => {
-                if (cachedResponse) {
-                    return cachedResponse;
-                }
-
-                return caches.open(RUNTIME).then((cache) => {
-                    return fetch(event.request).then((response) => {
-                        // Put a copy of the response in the runtime cache.
-                        return cache.put(event.request, response.clone()).then(() => {
-                            return response;
-                        });
-                    });
-                });
-            })
+            loadFromOrigin(event.request)
+            .then((response) => addToCache(event.request, response))
+            .catch(() => loadFromCache(event.request))
         );
     }
 });
